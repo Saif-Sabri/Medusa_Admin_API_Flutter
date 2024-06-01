fdd564e918679cdddd1fb2a22787c06ed493d385
Saif <sabri0saif@gmail.com>
2024-06-01 20:43:35 +0100

    get inspirations

--- lib/src/data/models/store_models/store/inspiration.dart
+++ lib/src/data/models/store_models/store/inspiration.dart
@@ -1,14 +1,59 @@
 class Inspiration {
-  const Inspiration({
-    required this.thumbnail,
+  String id;
+  DateTime createdAt;
+  DateTime updatedAt;
+  dynamic deletedAt;
+  String title;
+  String? subtitle;
+  String description;
+  String handle;
+  String styles;
+  String? thumbnail;
+  int price;
+
+  Inspiration({
     required this.id,
+    required this.createdAt,
+    required this.updatedAt,
+    required this.deletedAt,
+    required this.title,
+    required this.subtitle,
+    required this.description,
+    required this.handle,
+    required this.styles,
+    required this.thumbnail,
+    required this.price,
   });
 
-  factory Inspiration.fromJson(Map<String, dynamic> map) => Inspiration(
-        thumbnail: map["thumbnail"]!.toString(),
-        id: map["_id"]!.toString(),
-      );
+  factory Inspiration.fromJson(Map<String, dynamic> json) {
+    return Inspiration(
+      id: json['id'] as String,
+      createdAt: DateTime.parse(json['created_at'] as String),
+      updatedAt: DateTime.parse(json['updated_at'] as String),
+      deletedAt: json['deleted_at'],
+      title: json['title'] as String,
+      subtitle: json['subtitle'] as String?,
+      description: json['description'] as String,
+      handle: json['handle'] as String,
+      styles: json['styles'] as String,
+      thumbnail: json['thumbnail'] as String?,
+      price: json['price'] as int,
+    );
+  }
 
-  final String id;
-  final String thumbnail;
+  Map<String, dynamic> toJson() {
+    return {
+      'id': id,
+      'created_at': createdAt.toString(),
+      'updated_at': updatedAt.toString(),
+      'deleted_at': deletedAt,
+      'title': title,
+      'subtitle': subtitle,
+      'description': description,
+      'handle': handle,
+      'styles': styles,
+      'thumbnail': thumbnail,
+      'price': price,
+    };
+  }
 }
